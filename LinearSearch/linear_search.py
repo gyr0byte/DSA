a3febd051c66bc01ab8b3d25d1a28ffee1a5a0ae
Gauravlovescode <gqurav69@gmail.com>
2026-03-27 08:14:51 +0545

    Fix locate_card function to correctly return -1 when the query is not found

--- LinearSearch/linear_search.py
+++ LinearSearch/linear_search.py
@@ -2,8 +2,7 @@ def locate_card(card, query):
     for i in range(len(card)):
         if card[i] == query:
             return i
-        i += 1
-        return -1
+    return -1
 card = [11, 13, 9, 7, 4, 2, 3, 1]   
 query = 7
 result = locate_card(card, query)
