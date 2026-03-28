cac07daaa2f8a885598ae31413cf525020d46a5c
Gauravlovescode <gqurav69@gmail.com>
2026-03-28 15:23:45 +0545

    Add first and last position functions for binary search implementation

---
+++ BinarySearch/first_last_index_arr.py
@@ -0,0 +1,34 @@
+def first_position(nums, target):
+    low, high = 0, len(nums) - 1 
+    result = -1
+    while low <= high:
+        mid = ( low + high ) // 2
+
+        if nums[mid] == target:
+            result = mid
+            high = mid - 1
+        elif nums[mid] > target:
+            high = mid - 1
+        elif nums[mid] < target:
+            low = mid + 1
+            
+    return result
+
+def last_position(nums, target):
+    low, high = 0, len(nums) - 1
+    result = -1
+    while low <= high:
+        mid = ( low + high ) // 2
+        if nums[mid] == target:
+            result = mid
+            low = mid + 1
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return result
+nums = [1, 1, 2, 3, 4, 4, 6, 6, 6, 6, 7, 8, 8, 9]
+target = 6
+first_index = first_position(nums, target)
+last_index = last_position(nums, target)
+print(f"The starting index of target {target} is {first_index} and last index is {last_index}")
