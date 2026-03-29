8417a4df444321dcd041db7151075b1d28cbf16b
Gauravlovescode <gqurav69@gmail.com>
2026-03-29 21:44:18 +0545

    Add implementation of first position function for binary search

---
+++ BinarySearch/first_last_index_arr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int first_position(int nums[], int size, int target) {
+    int low = 0, high = size - 1, result = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            result = mid;
+            high = mid - 1;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return result;
+}
