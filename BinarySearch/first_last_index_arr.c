4198903bc0f876904f8bfaad2587268e37a7ba0b
Gauravlovescode <gqurav69@gmail.com>
2026-03-29 21:44:52 +0545

    Add implementation of last position function for binary search

--- BinarySearch/first_last_index_arr.c
+++ BinarySearch/first_last_index_arr.c
@@ -15,3 +15,19 @@ int first_position(int nums[], int size, int target) {
     }
     return result;
 }
+
+int last_position(int nums[], int size, int target) {
+    int low = 0, high = size - 1, result = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            result = mid;
+            low = mid + 1;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return result;
+}
