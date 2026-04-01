1d28a32578304d42c296f3d8591321a2c7298f19
Gauravlovescode <gqurav69@gmail.com>
2026-04-01 12:09:31 +0545

    Fix return value for unrotated list in count_rotation_linear function

--- RotationCount/rotation_count_linear.py
+++ RotationCount/rotation_count_linear.py
@@ -3,11 +3,11 @@ def count_rotation_linear(nums):
         if i > 0 and nums[i] < nums[i - 1]:
             return i
         
-    return -1 
+    return 0 
 
 nums = [4, 5, 6, 7, 1, 2, 3]
 result = count_rotation_linear(nums)
-if (result != -1):
+if result > 0:
     print(f"The list has been rotated {result} times!")
 else:
     print(f"The list has not been rotated!")
