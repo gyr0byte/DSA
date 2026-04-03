79e13eb18f2724a7ba0756a0fa53f9097102d62e
Gauravlovescode <gqurav69@gmail.com>
2026-04-03 21:40:25 +0545

    Add TreeNode class and sample instances for binary tree implementation

---
+++ BinaryTree/binarytee.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+node0 = TreeNode(3)
+node1 = TreeNode(4)
+node2 = TreeNode(5)
+
+print(node0.key)
+
