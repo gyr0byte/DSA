43f622c3d43655585483a9d90b9134339b8aad2c
Gauravlovescode <gqurav69@gmail.com>
2026-04-03 21:42:05 +0545

    Initialize binary tree structure with root node and children

--- BinaryTree/binarytee.py
+++ BinaryTree/binarytee.py
@@ -8,6 +8,8 @@ class TreeNode:
 node0 = TreeNode(3)
 node1 = TreeNode(4)
 node2 = TreeNode(5)
-
+tree = node0 # root node 
 print(node0.key)
 
+node0.left = node1 # children of node0
+node0.right = node2 # children of node0
