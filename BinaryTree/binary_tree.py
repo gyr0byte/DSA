641f9f91fef21f86bad6ffd3191476881ead0361
Gauravlovescode <gqurav69@gmail.com>
2026-04-03 21:44:55 +0545

    Enhance print statements for clarity in binary tree implementation

--- BinaryTree/binary_tree.py
+++ BinaryTree/binary_tree.py
@@ -9,7 +9,9 @@ node0 = TreeNode(3)
 node1 = TreeNode(4)
 node2 = TreeNode(5)
 tree = node0 # root node 
-print(node0.key)
+print(f"Root key/node: {node0.key}")
 
 node0.left = node1 # children of node0
 node0.right = node2 # children of node0
+print(f"Left key: {tree.left.key}")
+print(f"Right key: {tree.right.key}")
