95f61bad3c0005d1e7520812d873bcbcc868f5e3
Gauravlovescode <gqurav69@gmail.com>
2026-04-04 21:06:10 +0545

    Add nmbr_of_nodes.py with TreeNode class and functions for tree size calculation and tuple parsing

---
+++ BinaryTree/nmbr_of_nodes.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def tree_size(node):
+    if node is None:
+        return 0
+    return 1 + tree_size(node.left) + tree_size(node.right)
+    
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
+
+tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+tree1 = parse_tuple(tree_tuple)
+number_of_nodes = tree_size(tree1)
