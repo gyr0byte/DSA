43b986b06e908f062e821a9849e3c867e8cfba03
Gauravlovescode <gqurav69@gmail.com>
2026-04-04 22:07:55 +0545

    Add min_depth.py with TreeNode class, min depth calculation, and tuple parsing

---
+++ BinaryTree/min_depth.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def minDepth(self, root):
+        if root is None:
+            return 0
+        if root.left is None:
+            return 1 + self.minDepth(root.right)
+        if root.right is None:
+            return 1 + self.minDepth(root.left)
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
+
+tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+tree1 = parse_tuple(tree_tuple)
+solution = maxDepth(tree1)
+print(solution)
