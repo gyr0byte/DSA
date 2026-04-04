d59a05bbc0b8a008807fe0f1b7e4a7e45fdc7bcd
Gauravlovescode <gqurav69@gmail.com>
2026-04-04 22:05:43 +0545

    Refactor max_depth.py to include parse_tuple function for tuple parsing

--- BinaryTree/max_depth.py
+++ BinaryTree/max_depth.py
@@ -8,3 +8,15 @@ def maxDepth(self, root):
         if root is None:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
