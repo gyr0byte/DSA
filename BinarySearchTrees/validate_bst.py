3861796617e0c22dc011e087704489b7fc89c4e9
Gauravlovescode <gqurav69@gmail.com>
2026-04-06 21:41:04 +0545

    Add validate_bst.py with TreeNode class and tuple parsing function

---
+++ BinarySearchTrees/validate_bst.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
