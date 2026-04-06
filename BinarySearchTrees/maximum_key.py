02eb17f77de7d51d394915a562545bb8bf8ab089
Gauravlovescode <gqurav69@gmail.com>
2026-04-06 22:28:25 +0545

    Fix parse_tuple function to correctly reference left and right children of TreeNode

--- BinarySearchTrees/maximum_key.py
+++ BinarySearchTrees/maximum_key.py
@@ -6,10 +6,18 @@ class TreeNode:
 
 
 def parse_tuple(data):
-    if isinstance(data, tuple) and len(data) == 3: 
+    if isinstance(data, tuple) and len(data) == 3:
         node = TreeNode(data[1])
-        node.left = parse_tuple(node[0])
-        node.right = parse
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+
+    else:
+        node = TreeNode(data)
+
+    return node
 
 
 tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
