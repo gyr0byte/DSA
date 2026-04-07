f90d7d32473d3fbad36ecb32edc30e729bb94362
Gauravlovescode <gqurav69@gmail.com>
2026-04-07 08:46:32 +0545

    Add minimum_key.py with TreeNode class and functions to parse tuple and find minimum value

---
+++ BinarySearchTrees/minimum_key.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+
+    elif data is None:
+        node = None
+
+    else:
+        node = TreeNode(data)
+
+    return node
+
+
+def is_min(node):
+    if not node:
+        return float("inf")
+
+    return min(node.key, is_min(node.left), is_min(node.right))
+
+
+tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+tree1 = parse_tuple(tree_tuple)
+minvalue = is_min(tree1)
+print(f"The min value of the tree is {minvalue}")
