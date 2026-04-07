08a8e6950c0e3b8db56b4e880a9b2bcb372b0d5f
Gauravlovescode <gqurav69@gmail.com>
2026-04-07 18:45:43 +0545

    Fix parse_tuple condition and improve is_bst function for clarity

--- BinarySearchTrees/str_bst.py
+++ BinarySearchTrees/str_bst.py
@@ -4,8 +4,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 def parse_tuple(data):
-    if isinstance(data, tuple) and data == 3:
+    if isinstance(data, tuple) and len(data) == 3:
         node = TreeNode(data[1])
         node.left = parse_tuple(data[0])
         node.right = parse_tuple(data[2])
@@ -17,14 +18,19 @@ def parse_tuple(data):
         node = TreeNode(data)
     return node
 
-def is_bst(node, left, right):
+
+def is_bst(node, left = None, right = None):
     if not node:
         return True
-    elif not (node.key < right and node.key > left):
+
+    if left is not None and node.key <= left:  
         return False
-    
+    if right is not None and node.key >= right:
+        return False
     return (is_bst(node.left, left, node.key) and is_bst(node.right, node.key, right))
 
-tree1 = parse_tuple((("aakash", "biraj", "hemanth"), "jadesh", ("siddhant", "sonaksh", "vishal")))
 
-
+tree1 = parse_tuple((("aakash", "biraj", "hemanth"),
+                    "jadesh", ("siddhant", "sonaksh", "vishal")))
+result = is_bst(tree1)
+print(result)
