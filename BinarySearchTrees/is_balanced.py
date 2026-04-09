9e19f62e811d7092dce92e081f54369f34aa9361
Gauravlovescode <gqurav69@gmail.com>
2026-04-09 19:09:39 +0545

    Add is_balanced function to check tree balance and improve formatting in is_balanced.py

--- BinarySearchTrees/is_balanced.py
+++ BinarySearchTrees/is_balanced.py
@@ -1,9 +1,12 @@
 import display as ds
+
+
 class TreeNode():
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
+
 
 def parse_tuple(data):
     if isinstance(data, tuple) and data == 3:
@@ -17,6 +20,18 @@ def parse_tuple(data):
         node = TreeNode(data)
     return node
 
+
+def is_balanced(node):
+    if node is None:
+        return True, 0
+    balanced_l, height_l = is_balanced(node.left)
+    balanced_r, height_r = is_balanced(node.right)
+    balanced = balanced_l and balanced_r and abs(height_l - height_r) <= 1
+    height = 1 + max(height_l, height_r)
+    return balanced, height
+
+
 tree1 = parse_tuple((("aakash", "biraj", "hemanth"),
                     "jadesh", ("siddhant", "sonaksh", "vishal")))
 ds.display_keys(tree1)
+print(is_balanced(tree1))
