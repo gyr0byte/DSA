1fff317220f5c8ef68e1fad37c65a3903e84dce1
Gauravlovescode <gqurav69@gmail.com>
2026-04-11 22:52:35 +0545

    Fix get_index function to use length of data_list for index calculation

--- HashTables/list_kv.py
+++ HashTables/list_kv.py
@@ -3,12 +3,16 @@ def get_index(data_list, a_string):
     for a_character in a_string:
         a_number = ord(a_character)
         result += a_number
-    
-    list_index = result % data_list
+
+    list_index = result % len(data_list)
     return list_index
+
 
 data_list = [None] * 4096
 key, value = "Gaurav", "9811100022"
+key, value = "Sirjan", "9811122200"
 idx = get_index(data_list, key)
 data_list[idx] = (key, value)
 key, value = data_list[idx]
+keys = [key[0] for key in data_list if key is not None]
+print(keys)
