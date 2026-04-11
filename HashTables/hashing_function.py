e45af475f0ba3e2240bb91a87a55ff3b25a7ff28
Gauravlovescode <gqurav69@gmail.com>
2026-04-11 21:32:30 +0545

    Rename parameter in get_index function for consistency in hashing_function.py

--- HashTables/hashing_function.py
+++ HashTables/hashing_function.py
@@ -1,8 +1,8 @@
-def get_index(list_data, a_string):
+def get_index(data_list, a_string):
     result = 0
     for a_character in a_string:
         a_number = ord(a_character)
         result += a_number
         
-    list_index = result % len(list_data)
+    list_index = result % len(data_list)
     return list_index
