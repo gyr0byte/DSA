7194f9b734ed3bf828f642f6059161463362b168
Gauravlovescode <gqurav69@gmail.com>
2026-04-11 21:46:30 +0545

    Add get_index function to compute index based on string characters in insert.py

---
+++ HashTables/insert.py
@@ -0,0 +1,10 @@
+def get_index(data_list, a_string):
+    result = 0
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+        
+    list_index = result % len(data_list)
+    return list_index
+
+data_list = [None] * 4096
