fa3d28b1567767095d7adcc6d4a0487c48b28696
Gauravlovescode <gqurav69@gmail.com>
2026-04-13 09:32:30 +0545

    Refactor BasicHashTable methods for consistency and improve list method naming

--- HashTables/basic_hash_table.py
+++ HashTables/basic_hash_table.py
@@ -1,45 +1,53 @@
 MAX_HASH_TABLE_SIZE = 4096
+
+
 class BasicHashTable:
-    def __init__(self, max_size = MAX_HASH_TABLE_SIZE):
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE):
         self.data_list = [None] * max_size
-        
+
     def insert(self, key, value):
         idx = get_index(self.data_list, key)
         self.data_list[idx] = key, value
-        
+
     def find(self, key):
         idx = get_index(self.data_list, key)
         kv = self.data_list[idx]
 
         if kv is None:
             return None
-        else: 
+        else:
             key, value = kv
             return value
-        
+
     def update(self, key, value):
         idx = get_index(self.data_list, key)
         kv = self.data_list[idx]
         if kv is None:
-            return False          
+            return False
         self.data_list[idx] = key, value
-        return True               
+        return True
 
-    def list(self):
+    def list_all(self):
         return [kv[0] for kv in self.data_list if kv is not None]
-        
-    
+
+
 def get_index(data_list, a_string):
     result = 0
     for a_character in a_string:
         a_number = ord(a_character)
         result += a_number
-        
+
     list_index = result % len(data_list)
     return list_index
+
 
 basic_table = BasicHashTable(max_size=1024)
 basic_table.insert('Gaurav', '9800022211')
 basic_table.insert('Sirjan', '9822334455')
 
 print(basic_table.find('Gaurav'))
+print(basic_table.find('Sirjan') + '\n')
+
+basic_table.update('Sirjan', '9811111111')
+print(basic_table.find('Sirjan'))
+print(basic_table.list_all())
