20d9df4872671b194ac1301fe7c9f0120e467ff6
Gauravlovescode <gqurav69@gmail.com>
2026-04-13 09:15:19 +0545

    Add BasicHashTable class with insert and get_index methods

---
+++ HashTables/basic_hash_table.py
@@ -0,0 +1,18 @@
+class BasicHashTable:
+    def __init__(self, max_size = MAX_HASH_TABLE_SIZE):
+        self.data_list = [None] * max_size
+        
+    def insert(self, key, value):
+        idx = get_index(self.data_list, key)
+        self.data_list[idx] = value
+        
+    
+
+def get_index(data_list, a_string):
+    result = 0
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+        
+    list_index = result * len(data_list)
+    return list_index
