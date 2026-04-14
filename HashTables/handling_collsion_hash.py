bc15ea43d6a0b2d6414c818570c41c3b9f8d56cd
Gauravlovescode <gqurav69@gmail.com>
2026-04-14 18:13:08 +0545

    Add get_index and get_valid_index functions for hash table indexing

---
+++ HashTables/handling_collsion_hash.py
@@ -0,0 +1,17 @@
+def get_index(data_list, a_string):
+    result = 0
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+        
+    list_index = result % len(data_list)
+    return list_index
+
+def get_valid_index(data_list, key):
+    idx = 
+    
+    
+data_list = [None] * 4096
+key, value = "Gaurav", "9811100022"
+idx = get_index(data_list, key)
+data_list[idx] = key, value
