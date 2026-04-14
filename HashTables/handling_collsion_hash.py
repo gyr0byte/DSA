e126acf062c2e257e9148375910ceaa68edf3a4e
Gauravlovescode <gqurav69@gmail.com>
2026-04-14 18:18:52 +0545

    Refactor get_index usage to ensure valid index retrieval in hash table

--- HashTables/handling_collsion_hash.py
+++ HashTables/handling_collsion_hash.py
@@ -3,29 +3,37 @@ def get_index(data_list, a_string):
     for a_character in a_string:
         a_number = ord(a_character)
         result += a_number
-        
+
     list_index = result % len(data_list)
     return list_index
 
+
 def get_valid_index(data_list, key):
     idx = get_index(data_list, key)
-    
+
     while True:
         kv = data_list[idx]
 
         if kv is None:
             return idx
-        
+
         k, v = kv
         if k == key:
             return idx
-        
+
         idx += 1
         if idx == len(data_list):
             idx = 0
-    
-    
+
+
 data_list = [None] * 4096
 key, value = "Gaurav", "9811100022"
-idx = get_index(data_list, key)
+idx = get_valid_index(data_list, key)
 data_list[idx] = key, value
+
+key, value = "Gaaruv", "9811100000"
+idx = get_valid_index(data_list, key)
+data_list[idx] = key, value
+
+keys = [key[0] for key in data_list if key is not None]
+print(keys)
