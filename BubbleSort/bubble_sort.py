a39a4a9ce64621c8df504eafa243df1c660e5f43
Gauravlovescode <gqurav69@gmail.com>
2026-04-15 21:20:47 +0545

    Fix bubble_sort implementation by correcting loop structure and adding example usage

--- BubbleSort/bubble_sort.py
+++ BubbleSort/bubble_sort.py
@@ -1,6 +1,10 @@
 def bubble_sort(nums):
-    for i in range(len(range)-1):
-        if nums[i] > nums[i+1]:
-            nums[i], nums[i+1] = nums[i+1], nums[i]
-            
+    for j in range(len(nums) - 1):
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+
     return nums
+
+nums = [5, 7, 2, 1, 6, 8, 3, 4]
+print(bubble_sort(nums))
