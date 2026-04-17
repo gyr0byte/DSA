c709053a3dc1822de083e2dca05d1583a3e3c20c
Gauravlovescode <gqurav69@gmail.com>
2026-04-17 22:24:44 +0545

    Implement merge function to combine two sorted lists in merge_sort.py

--- MergeSort/merge_sort.py
+++ MergeSort/merge_sort.py
@@ -1,6 +1,17 @@
-from heapq import merge
+def merge(nums1, nums2):
+    merged = []
 
-
+    i,j = 0,0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] <= nums2[j]:
+            merged.append(nums1[i])
+            i += 1
+        else:
+            merged.append(nums2[j])
+            j += 1
+            
+    
+    
 def merge_sort(nums):
     if len(nums) <= 1:
         return nums
