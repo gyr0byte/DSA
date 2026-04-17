b4e641a03d3779fea5e675ac885891c12436c358
Gauravlovescode <gqurav69@gmail.com>
2026-04-17 22:11:23 +0545

    Refactor merge_sort function to improve readability and structure

--- MergeSort/merge_sort.py
+++ MergeSort/merge_sort.py
@@ -1,5 +1,17 @@
+from heapq import merge
+
+
 def merge_sort(nums):
     if len(nums) >= 1:
         return nums
     
     mid = len(nums) // 2
+    
+    left = nums[:mid]
+    right = nums[mid:]
+
+    left_sorted, right_sorted = merge_sort(left), merge_sort(right)
+
+    sorted_nums = merge(left_sorted, right_sorted)
+
+    return sorted_nums
