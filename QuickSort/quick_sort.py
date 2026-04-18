fc65ead1787334827bb1e8a564bf592af1188e82
Gauravlovescode <gqurav69@gmail.com>
2026-04-18 11:36:30 +0545

    Refactor quick_sort to conditionally set end parameter for clarity

--- QuickSort/quick_sort.py
+++ QuickSort/quick_sort.py
@@ -1,5 +1,6 @@
 def quick_sort(nums, start = 0, end = None):
-    end = len(nums) - 1
+    if end is None:
+        end = len(nums) - 1
     if start < end:
         pivot = partition(nums, start, end)
         quick_sort(nums, start, pivot-1)
