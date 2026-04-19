ab9226639794f1738f6df805a16ddcd4c2a78509
Gauravlovescode <gqurav69@gmail.com>
2026-04-19 18:10:56 +0545

    Implement base case and matching condition in lcs_recursive function

--- DynanicProgramming/common_subsequence.py
+++ DynanicProgramming/common_subsequence.py
@@ -2,3 +2,7 @@ def lcs_recursive(seq1, seq2, idx1 = 0, idx2 = 0):
     if idx1 == len(seq1) or idx2 == len(seq2):
         return 0
     
+    elif seq1[idx1] == seq2[idx2]:
+        return 1 + lcs_recursive(seq1, seq2, idx1 + 1, idx2 + 1)
+    
+    
