9f20261f0f4ba6c20b2bffc657b8e1778bc6973c
Gauravlovescode <gqurav69@gmail.com>
2026-04-19 18:13:19 +0545

    Complete lcs_recursive function implementation with recursive options for mismatched characters

--- DynanicProgramming/common_subsequence.py
+++ DynanicProgramming/common_subsequence.py
@@ -5,4 +5,8 @@ def lcs_recursive(seq1, seq2, idx1 = 0, idx2 = 0):
     elif seq1[idx1] == seq2[idx2]:
         return 1 + lcs_recursive(seq1, seq2, idx1 + 1, idx2 + 1)
     
+    else:
+        option1 = lcs_recursive(seq1, seq2, idx1 + 1, idx2)
+        option2 = lcs_recursive(seq1, seq2, idx1, idx2 + 1)
+        return max(option1, option2)
     
