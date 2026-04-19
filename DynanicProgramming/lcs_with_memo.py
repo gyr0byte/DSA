e50049323e01552bcf8672754d62e12b660054f4
Gauravlovescode <gqurav69@gmail.com>
2026-04-19 20:47:16 +0545

    Fix lcs_memo function by adding missing return statements for base cases

--- DynanicProgramming/lcs_with_memo.py
+++ DynanicProgramming/lcs_with_memo.py
@@ -4,6 +4,10 @@ def lcs_memo(seq1, seq2):
         key = (idx1, idx2)
         if key in memo:
             return memo[key]
+        
         elif idx1 == len(seq1) or idx2 == len(seq2):
             memo[key] = 0
+        
+        elif seq1[idx1] == seq2[idx2]:
+            memo[key] = 1 + recurse(idx1 + 1, idx2 + 1)
         
