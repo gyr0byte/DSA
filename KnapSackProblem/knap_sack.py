f6eb693ccb40bf9d24856be56d820c7443b85d17
Gauravlovescode <gqurav69@gmail.com>
2026-04-20 21:06:46 +0545

    Complete max_profit_recursive function implementation in knap_sack.py

--- KnapSackProblem/knap_sack.py
+++ KnapSackProblem/knap_sack.py
@@ -3,5 +3,7 @@ def max_profit_recursive(weights, profits, capacity, idx=0):
         return 0
     elif weights[idx] > capacity:
         return max_profit_recursive(weights, profits, capacity, idx+1)
-    
-    
+    else:
+        option1 = max_profit_recursive(weights, profits, capacity, idx+1)
+        option1 = profits[idx] + max_profit_recursive(weights, profits, capacity - weights[idx], idx + 1)
+        
