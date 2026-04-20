e765a89a1ec41918eb06ad187e50361dc7b3d6aa
Gauravlovescode <gqurav69@gmail.com>
2026-04-20 21:39:11 +0545

    Implement dynamic programming logic in max_profit_dp function

--- KnapSackProblem/knap_sack_dp.py
+++ KnapSackProblem/knap_sack_dp.py
@@ -1,3 +1,10 @@
 def max_profit_dp(weights, profit, capacity):
     n = len(weights)
     table = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    for i in range(n):
+        for c in range(1, capacity):
+            if weights[i] > c:
+                table[i+1][c] = table[i][c]
+            else:
+                table[i+1][c] = max(table[i][c], profit[i] + table[i][c-weights[i]])
+    return table[-1][-1]
