e5847e0aaa3006727db13400459c761403c0f7a1
Gauravlovescode <gqurav69@gmail.com>
2026-04-20 21:08:27 +0545

    Fix max_profit_recursive function by correcting option2 assignment and adding test cases

--- KnapSackProblem/knap_sack.py
+++ KnapSackProblem/knap_sack.py
@@ -5,5 +5,9 @@ def max_profit_recursive(weights, profits, capacity, idx=0):
         return max_profit_recursive(weights, profits, capacity, idx+1)
     else:
         option1 = max_profit_recursive(weights, profits, capacity, idx+1)
-        option1 = profits[idx] + max_profit_recursive(weights, profits, capacity - weights[idx], idx + 1)
-        
+        option2 = profits[idx] + max_profit_recursive(weights, profits, capacity - weights[idx], idx + 1)
+        return max(option1, option2)
+
+print(max_profit_recursive([1, 2, 3], [10, 15, 40], 6))
+print(max_profit_recursive([1, 2, 3], [10, 15, 40], 5))
+print(max_profit_recursive([1, 2, 3], [10, 15, 40], 3))
