d4eefec8a81a19918e6b6b2200f184ce20b7af63
gyr0byte <gqurav69@gmail.com>
2026-04-21 20:21:39 +0545

    Fix bfs function to use graph.data for discovered, distance, and parent arrays

--- PathFindingAlgorithms/breadth_first_search.py
+++ PathFindingAlgorithms/breadth_first_search.py
@@ -15,10 +15,13 @@ class Graph:
 
 def bfs(graph, root):
     queue = []
-    discovered = [False] * graph.num_nodes
-
+    discovered = [False] * len(graph.data)
+    distance = [None] * len(graph.data)
+    parent = [None] * len(graph.data)
+    
     discovered[root] = True
     queue.append(root)
+    distance[root] = 0
     idx = 0
 
     while idx < len(queue):
@@ -29,10 +32,11 @@ def bfs(graph, root):
         # check all edges of current
         for node in graph.data[current]:
             if not discovered[node]:
+                distance[node] = 1 + distance[current]
+                parent[node] = current
                 discovered[node] = True
                 queue.append(node)
-
-    return queue
+    return f"Queue: {queue}, Distance: {distance}, Parent: {parent}"
 
 
 num_nodes = 5
