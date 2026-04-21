c488db168a8caf8040f0c246f995c4a064242619
gyr0byte <gqurav69@gmail.com>
2026-04-21 18:34:33 +0545

    Fix bfs function to use graph.num_nodes and correct self.data reference

--- PathFindingAlgorithms/breadth_first_search.py
+++ PathFindingAlgorithms/breadth_first_search.py
@@ -12,24 +12,31 @@ class Graph:
     def __str__(self):
         return self.__repr__()
 
+
 def bfs(graph, root):
     queue = []
-    discovered = [False] * len(graph)
-    
+    discovered = [False] * graph.num_nodes
+
     discovered[root] = True
     queue.append(root)
     idx = 0
-    
+
     while idx < len(queue):
         # dequeue
         current = queue[idx]
         idx += 1
-        
+
         # check all edges of current
-        for node in self.data[current]:
+        for node in graph.data[current]:
             if not discovered[node]:
                 discovered[node] = True
                 queue.append(node)
 
     return queue
 
+
+num_nodes = 5
+edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
+graph = Graph(num_nodes, edges)
+print(graph)
+print(bfs(graph, 3))
