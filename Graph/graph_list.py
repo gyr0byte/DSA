242f4591c5b31ebd5fa80e22f6766f52b6a6418c
Gauravlovescode <gqurav69@gmail.com>
2026-04-21 17:17:45 +0545

    Implement Graph class with edge initialization and representation method

--- Graph/graph_list.py
+++ Graph/graph_list.py
@@ -2,3 +2,14 @@ class Graph:
     def __init__(self, num_nodes, edges):
         self.num_nodes = num_nodes
         self.data = [[] for _ in range(num_nodes)]
+        for n1, n2 in edges:
+            self.data[n1].append(n2)
+            self.data[n2].append(n1)
+    
+    def __repr__(self):
+        [enumerate(self.data)]
+            
+num_nodes = 5
+edges = [(0, 1), (0, 4), (1,2), (1, 3), (1, 4), (2,3), (3,4)]
+graph = Graph(num_nodes, edges)
+print(graph.data)
