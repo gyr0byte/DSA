efc9b299f9f4a03a469b66bc7a2842f35a1fbd21
gyr0byte <gqurav69@gmail.com>
2026-04-21 18:02:24 +0545

    Remove unnecessary newline in Graph class

--- Graph/graph_list.py
+++ Graph/graph_list.py
@@ -12,7 +12,6 @@ class Graph:
     def __str__(self):
         return self.__repr__()
 
-
 num_nodes = 5
 edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
 graph = Graph(num_nodes, edges)
