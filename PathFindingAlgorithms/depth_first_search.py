a2d25e3f6419f8a934b60bda39626432e4e09612
gyr0byte <gqurav69@gmail.com>
2026-04-22 08:55:03 +0545

    Refactor whitespace and formatting in depth_first_search.py for improved readability

--- PathFindingAlgorithms/depth_first_search.py
+++ PathFindingAlgorithms/depth_first_search.py
@@ -1,7 +1,7 @@
 class Graph:
     def __init__(self, num_nodes, edges):
         self.num_nodes = num_nodes
-        self.data = [[] for _ in range (num_nodes)]
+        self.data = [[] for _ in range(num_nodes)]
         for n1, n2 in edges:
             self.data[n1].append(n2)
             self.data[n2].append(n1)
@@ -11,23 +11,27 @@ class Graph:
 
     def __str__(self):
         return self.__repr__()
-        
+
+
 def dfs(graph, root):
     stack = []
     discovered = [False] * len(graph.data)
     result = []
-    
-    stack.append(root)        
 
+    stack.append(root)
     while len(stack) > 0:
         current = stack.pop()
-        discovered[current] = True
-        result.append(current)
-        for node in graph.data[current]:
-            stack.append(node)
-            
-    
+        if not discovered[current]:
+            discovered[current] = True
+            result.append(current)
+            for node in graph.data[current]:
+                if not discovered[node]:
+                    stack.append(node)
+
+    return result
+
 num_nodes = 5
 edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
 graph = Graph(num_nodes, edges)
 print(graph)
+print(dfs(graph, 3))
