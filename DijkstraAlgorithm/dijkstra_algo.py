3a07322bde881ad4696345ce7a1fb71b9e918652
gyr0byte <gqurav69@gmail.com>
2026-04-23 19:24:32 +0545

    Fix edge handling in Graph class for undirected edges and improve __repr__ method

--- DijkstraAlgorithm/dijkstra_algo.py
+++ DijkstraAlgorithm/dijkstra_algo.py
@@ -15,6 +15,20 @@ class Graph:
                     self.data[node2].append(node1)
                     self.weight[node2].append(weight)
             else:
+                # work without weights
+                node1, node2 = edge
+                self.data[node1].append(node2)
+                if not directed:
+                    self.data[node2].append(node1)
+    def __repr__(self):
+        result = ""
+        if self.weighted:
+            for i, (nodes, weights) in enumerate(zip(self.data, self.weight)):
+                result += f"{i}: {list(zip(nodes, weights))}"
+        else:
+            for i, nodes in enumerate(self.data):
+                result += f"{i}: {nodes}\n"
+        return result
         
 
 def shortest_path(graph, source, target):
