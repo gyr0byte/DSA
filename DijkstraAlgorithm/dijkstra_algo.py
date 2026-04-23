16c828e7d4292ec87fd22b6a1a1dd5be5d688048
gyr0byte <gqurav69@gmail.com>
2026-04-23 19:14:06 +0545

    Add Graph class initialization with directed and weighted parameters

--- DijkstraAlgorithm/dijkstra_algo.py
+++ DijkstraAlgorithm/dijkstra_algo.py
@@ -1,4 +1,14 @@
-
+class Graph:
+    def __init__(self, num_nodes, edges, directed = False, weighted = False):
+        self.num_nodes = num_nodes
+        self.directed = directed
+        self.weighted = weighted
+        self.data = [[] for _ in range(num_nodes)]
+        self.weight = [[] for _ in range(num_nodes)]
+        for edge in edges:
+            if self.weighted:
+                # include weight
+        
 
 def shortest_path(graph, source, target):
     visited = [False] * len(graph.data)
