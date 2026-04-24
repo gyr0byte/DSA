b7f139761f5a740301f29af1a79e88dde1eb7ba4
gyr0byte <gqurav69@gmail.com>
2026-04-24 12:59:36 +0545

    Refactor subarray_sum function for improved logic and readability

--- Questions/subarray_sum.py
+++ Questions/subarray_sum.py
@@ -1,5 +1,14 @@
 def subarray_sum(arr, target):
-    
+    j = 0
+    for i in range(len(arr)):
+        s = 0
+        if sum(arr[j:i]) > target:
+            j += 1
+        elif sum(arr[j:i]) == target:
+            return j, i
+    return 0
+
 
 arr = [1, 7, 4, 2, 1, 3, 11, 5]
 target = 10
+print(subarray_sum(arr, target))
