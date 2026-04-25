42980abe4f75ec37a1d8338dbc6b010c8231d96a
gyr0byte <gqurav69@gmail.com>
2026-04-25 12:00:13 +0545

    Implement memoization for min_steps function to optimize performance

--- Questions/minimum_update_distance.py
+++ Questions/minimum_update_distance.py
@@ -1,3 +1,4 @@
+# solution using recursion
 def min_steps(str1, str2, i1 = 0, i2 = 0):
     if i1 == len(str1):
         return len(str2) - i2
@@ -10,6 +11,19 @@ def min_steps(str1, str2, i1 = 0, i2 = 0):
                        min_steps(str1, str2, i1 + 1, i2 + 1), # swap
                        min_steps(str1, str2, i1, i2 + 1)) #inserted
 
+# solution using memoization
+def min_steps_memo(str1, str2):
+    memo = {}
+    def recurse(i1, i2):
+        key = i1, i2
+        if key in memo:
+            return memo[key]
+        elif i1 == len(str1):
+            memo[key] = len(str2) - i2
+        elif i2 = len(str2):
+            memo[key] = len(str1) - i1
+        
+            
 str1 = "intention"
 str2 = "execution"
 print(min_steps(str1, str2))
