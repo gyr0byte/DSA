96f8f574eb4574eb3c30faf56c241e2d6865f224
gyr0byte <gqurav69@gmail.com>
2026-04-25 12:03:47 +0545

    Fix minor issues in min_steps function and improve readability

--- Questions/minimum_update_distance.py
+++ Questions/minimum_update_distance.py
@@ -9,7 +9,7 @@ def min_steps(str1, str2, i1 = 0, i2 = 0):
     else:
         return 1 + min(min_steps(str1, str2, i1 + 1, i2), # deletion
                        min_steps(str1, str2, i1 + 1, i2 + 1), # swap
-                       min_steps(str1, str2, i1, i2 + 1)) #inserted
+                       min_steps(str1, str2, i1, i2 + 1)) # insertion
 
 # solution using memoization
 def min_steps_memo(str1, str2):
@@ -20,10 +20,14 @@ def min_steps_memo(str1, str2):
             return memo[key]
         elif i1 == len(str1):
             memo[key] = len(str2) - i2
-        elif i2 = len(str2):
+        elif i2 == len(str2):
             memo[key] = len(str1) - i1
-        
-            
+        elif str1[i1] == str2[i2]:
+            memo[key] = recurse(i1+1, i2+1)
+        else:
+            memo[key] = 1 + min(recurse(i1 + 1, i2), # deletion
+                                recurse(i1 + 1, i2 + 1), # swap
+                                recurse(i1, i2 + 1))  # insertion 
 str1 = "intention"
 str2 = "execution"
 print(min_steps(str1, str2))
