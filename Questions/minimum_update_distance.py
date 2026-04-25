49fc07a7c4e21844bfbcd93d448846dc7a321e7e
gyr0byte <gqurav69@gmail.com>
2026-04-25 11:27:01 +0545

    Fix min_steps function to handle insertion case and improve readability

--- Questions/minimum_update_distance.py
+++ Questions/minimum_update_distance.py
@@ -5,7 +5,11 @@ def min_steps(str1, str2, i1 = 0, i2 = 0):
         return len(str1) - i1
     elif str1[i1] == str2[i2]:
         return min_steps(str1, str2, i1 + 1, i2 + 1)
-    
+    else:
+        return 1 + min(min_steps(str1, str2, i1 + 1, i2), # deletion
+                       min_steps(str1, str2, i1 + 1, i2 + 1), # swap
+                       min_steps(str1, str2, i1, i2 + 1)) #inserted
 
 str1 = "intention"
 str2 = "execution"
+print(min_steps(str1, str2))
